5c828cdc7fad33cd28f2e5f4a2c5889384eefb9d
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-03 13:26:01 +0530

    Create auth middleware

---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const auth = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.status(401).json({
+      success: false,
+      message: "Unauthorized access",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET);
+    req.user = decoded.sub;
+    next();
+  } catch (error) {
+    console.error(error.message);
+    return res.status(401).json({
+      success: false,
+      message: "Invalid or expired token",
+    });
+  }
+};
+
+export default auth;
