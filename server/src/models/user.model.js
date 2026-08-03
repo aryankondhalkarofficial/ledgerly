a958b1164cbec7d0690428a0ded2abb278336987
Aryan Kondhalkar <aryankondhalkar52@gmail.com>
2026-08-03 12:49:15 +0530

    Create user model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      minlength: [2, "Name should be at least 2 characters"],
+      maxlength: [50, "Name cannot exceed 50 characters"],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [8, "Password should be at least 8 characters"],
+      maxlength: [64, "Password cannot exceed 64 characters"],
+    },
+    currency: {
+      type: String,
+      enum: ["INR", "USD", "EUR"],
+      default: "INR",
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
